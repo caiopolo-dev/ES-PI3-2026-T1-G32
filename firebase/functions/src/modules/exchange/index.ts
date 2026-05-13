15b47c9c9b2888817f54786d70ce8d30f3cff467
Rafael Mendes Valente <rafaelmvalente@gmail.com>
2026-05-13 16:05:48 -0300

    feat: exportações públicas do domínio de exchange

--- firebase/functions/src/modules/exchange/index.ts
+++ firebase/functions/src/modules/exchange/index.ts
@@ -1,2 +1,6 @@
-// Exportações públicas do domínio de câmbio
-// export { getExchange, exchangeRepository, ExchangeRate, ... }
+// Autor: Rafael Mendes Valente
+// Descrição: Exportações públicas do domínio de exchange
+export {buyToken} from "./handlers/buyToken";
+export {sellToken} from "./handlers/sellToken";
+export {getWalletInfo} from "./handlers/getWalletInfo";
+export {getTokenHistory} from "./handlers/getTokenHistory";
