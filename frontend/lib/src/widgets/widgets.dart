51e10fdd8bf753a0123172c5cac03443015fd527
GustavoADSC <gu.siqueira10@hotmail.com>
2026-05-14 21:28:57 -0300

    feat: adicionar widgets reutilizáveis de depósito e exibição de saldo

---
+++ frontend/lib/src/widgets/widgets.dart
@@ -0,0 +1,9 @@
+// Autor: Gustavo Alves de Siqueira Costa
+// Data: 14/05/2026
+// Descrição: Barrel de widgets reutilizáveis do app
+
+export 'app_loading_indicator.dart';
+export 'deposit_dialog.dart';
+export 'main_scaffold.dart';
+export 'saldo_display.dart';
+export 'user_avatar_menu.dart';
